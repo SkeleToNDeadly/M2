e16a690ecf9fcc3d804f0f1d0611a984547d89bd
SkeleTonD <mr.daniil00@bk.ru>
2023-10-22 14:35:56 +0700

    Испавли некоторые ошибки

--- task1/2.1.c
+++ task1/2.1.c
@@ -3,6 +3,7 @@
 
 #define MAX_CONTACTS 100
 #define MAX_NAME_LENGTH 50
+#define MAX_LN_LENGTH 50
 #define MAX_WORK_LENGTH 100
 #define MAX_POS_LENGTH 100
 #define MAX_PHONE_LENGTH 20
@@ -11,7 +12,7 @@
 
 struct Contact {
     char first_name[MAX_NAME_LENGTH];
-    char last_name[MAX_NAME_LENGTH];
+    char last_name[MAX_LN_LENGTH];
     char work_place[MAX_WORK_LENGTH];
     char position[MAX_POS_LENGTH];
     char phone_numbers[MAX_PHONE_LENGTH];
@@ -25,10 +26,15 @@ int contactCount = 0;
 void addContact() {
     if (contactCount < MAX_CONTACTS) {
         struct Contact newContact;
-        printf("Введите ваше имя: ");
-        scanf("%s", newContact.first_name);
-        printf("Введите вашу фамилию: ");
-        scanf("%s", newContact.last_name);
+
+        do {
+            printf("Введите ваше имя: ");
+        } while (scanf("%50s", newContact.first_name) != 1);
+
+        do {
+            printf("Введите вашу фамилию: ");
+        } while (scanf("%50s", newContact.last_name) != 1);
+
         printf("Введите ваше место работы: ");
         scanf("%s", newContact.work_place);
         printf("Введите вашу должность: ");
@@ -42,21 +48,21 @@ void addContact() {
         contacts[contactCount++] = newContact;
         printf("Контакт был добавлен успешно!\n");
     } else {
-        printf("Телефонная книга запелнена. Невозможно добавить больше контактов.\n");
+        printf("Телефонная книга заполнена. Невозможно добавить больше контактов.\n");
     }
 }
 
 void displayContacts() {
-    printf("Контакты в телефонной книге:\n");
+    printf("КОНТАКТЫ В ТЕЛЕФОННОЙ КНИГЕ:\n");
     for (int i = 0; i < contactCount; i++) {
-        printf("Контакты %d:\n", i + 1);
+        printf("Контакт %d:\n", i + 1);
         printf("Имя: %s\n", contacts[i].first_name);
         printf("Фамилия: %s\n", contacts[i].last_name);
         printf("Место работы: %s\n", contacts[i].work_place);
         printf("Должность: %s\n", contacts[i].position);
         printf("Телефон: %s\n", contacts[i].phone_numbers);
         printf("Email: %s\n", contacts[i].email);
-        printf("Ссыла на соц.сеть: %s\n", contacts[i].social_links);
+        printf("Ссылка на соц.сеть: %s\n", contacts[i].social_links);
         printf("\n");
     }
 }
@@ -65,10 +71,10 @@ int main() {
     int choice;
     do {
         printf("Телефонная книга:\n");
-        printf("1. Добавить  контакт\n");
+        printf("1. Добавить контакт\n");
         printf("2. Показать существующие\n");
         printf("3. Выйти\n");
-        printf("Введите выше выбор: ");
+        printf("Введите ваш выбор: ");
         scanf("%d", &choice);
         switch (choice) {
             case 1:
@@ -81,7 +87,7 @@ int main() {
                 printf("Выход из телефонной книги.\n");
                 break;
             default:
-                printf("Укан неверный выбор. Пожалуйста попробуйте ещё раз.\n");
+                printf("Указан неверный выбор. Пожалуйста, попробуйте еще раз.\n");
         }
     } while (choice != 3);
 
